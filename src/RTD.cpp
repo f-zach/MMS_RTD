bc84b027534588fe62eaa767a9b03305d372fa9b
f-zach <63615390+f-zach@users.noreply.github.com>
2022-01-14 10:33:22 +0100

    Edited comments

--- src/RTD.cpp
+++ src/RTD.cpp
@@ -3,7 +3,6 @@
 RTDmodule::RTDmodule(int i2c_addressRTD)
     : MAX31865rtd(0), _i2c_addressRTD(i2c_addressRTD)
 {
-
 }
 
 void RTDmodule::config(byte sensorPositions, int mode, int Rref, int wiring = 1, int FaultDetection = 0, int filter = 1)
@@ -35,20 +34,20 @@ void RTDmodule::config(byte sensorPositions, int mode, int Rref, int wiring = 1,
 
     _sensorCount = 0;
 
-    for(int _i = 0; _i < 4; _i++)
-    {   
+    for (int _i = 0; _i < 4; _i++)
+    {
         // Check the sensor positions byte for the occupied slots
-        if(bitRead(_sensorPositions, _i))
+        if (bitRead(_sensorPositions, _i))
         {
             // Count number of sensors
             _sensorCount++;
 
-            // Make a slot number array 
-            _sensorNumAtPos[_sensorCount] = _i+1;
+            // Make a slot number array
+            _sensorNumAtPos[_sensorCount] = _i + 1;
         }
     }
     sensorCount = _sensorCount;
-    
+
     // Set all CS-pins of the module to high
     Wire.beginTransmission(_i2c_addressRTD);
     Wire.write(0xFF);
@@ -61,25 +60,27 @@ void RTDmodule::startMeasurementSingle(int sensorNum)
     //
     // sensorNum    Number of the sensor on which the measurement is to be started
 
-
     // Start a new measurement only if at least 65 milliseconds passed and the sensor was read already
-    if((millis() - _tMeasurementStart[sensorNum] >= 65) && _valueRead[sensorNum])
+    if ((millis() - _tMeasurementStart[sensorNum] >= 65) && _valueRead[sensorNum])
     {
-    _valueRead[sensorNum] = false;
+        _valueRead[sensorNum] = false;
 
-    // Pull down the CS pin of the defined MAX31865 and send the configuration byte to start a measurement.
-    // Set all CS pins to high after.
+        // Pull down the CS pin of the defined MAX31865 and send the configuration byte to start a measurement.
+        // Set all CS pins to high after.
 
-    Wire.beginTransmission(_i2c_addressRTD);
-    Wire.write(B11111111 ^ (B00000001 << _sensorNumAtPos[sensorNum]));
-    Wire.endTransmission();
+        // Set the CS pin of the according MAX31865 to low
+        Wire.beginTransmission(_i2c_addressRTD);
+        Wire.write(B11111111 ^ (B00000001 << _sensorNumAtPos[sensorNum]));
+        Wire.endTransmission();
 
-    MAX31865rtd.startMeasurement();
-    _tMeasurementStart[sensorNum] = millis();
+        // Send configuration byte to the sensor to start the measurement
+        MAX31865rtd.startMeasurement();
+        _tMeasurementStart[sensorNum] = millis();
 
-    Wire.beginTransmission(_i2c_addressRTD);
-    Wire.write(0xFF);
-    Wire.endTransmission();
+         // Set all CS pins back to high
+        Wire.beginTransmission(_i2c_addressRTD);
+        Wire.write(0xFF);
+        Wire.endTransmission();
     }
 }
 
@@ -87,7 +88,7 @@ void RTDmodule::startMesasurementAll()
 {
     // Starts measurement with all configured MAX31865s
 
-    for(int _i = 0; _i < _sensorCount; _i++)
+    for (int _i = 0; _i < _sensorCount; _i++)
     {
         startMeasurementSingle(_sensorNumAtPos[_i]);
     }
@@ -99,13 +100,16 @@ float RTDmodule::readTemperatureSingle(int sensorNum)
     //
     // sensorNum    Number of the sensor on which is to be read
 
+    // Set CS pin to low for the according MAX31865
     Wire.beginTransmission(_i2c_addressRTD);
     Wire.write(B11111111 ^ (B00000001 << _sensorNumAtPos[sensorNum]));
     Wire.endTransmission();
 
+    // Read the temperature value
     TemperatureSingle = MAX31865rtd.read();
     _valueRead[sensorNum] = true;
 
+    // Set all CS pins back to high
     Wire.beginTransmission(_i2c_addressRTD);
     Wire.write(0xFF);
     Wire.endTransmission();
@@ -117,8 +121,8 @@ void RTDmodule::readTemperatureAll()
 {
     // Read temperature from all configured MAX31865s
 
-    for(int _i = 0; _i < _sensorCount; _i++)
+    for (int _i = 0; _i < _sensorCount; _i++)
     {
-       TemperatureAll[_i] = readTemperatureSingle(_sensorNumAtPos[_i]);
+        TemperatureAll[_i] = readTemperatureSingle(_sensorNumAtPos[_i]);
     }
 }
